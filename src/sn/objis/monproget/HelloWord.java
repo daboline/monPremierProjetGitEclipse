ae71fde71a3aeb90616a764c9b37f6cdb08ab89f
daboline <boudabi89@hotmail.fr>
2018-09-06 16:03:31 +0000

    ajout de la méthode main

--- src/sn/objis/monproget/HelloWord.java
+++ src/sn/objis/monproget/HelloWord.java
@@ -1,9 +1,10 @@
 package sn.objis.monproget;
 
 public class HelloWord {
-
-	public HelloWord() {
-		// TODO Auto-generated constructor stub
+	public static void main(String args[]) {
+		System.out.println("Bonjour à tous promo5");
+		
 	}
 
+	
 }
