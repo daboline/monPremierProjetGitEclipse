00d82fd6be9701faf1d1999581e0992533cd8002
daboline <boudabi89@hotmail.fr>
2018-09-07 12:19:26 +0000

    Commit ajout Hello word

--- src/sn/objis/monproget/HelloWord.java
+++ src/sn/objis/monproget/HelloWord.java
@@ -3,5 +3,7 @@ package sn.objis.monproget;
 public class HelloWord {
 	public static void main(String args[]) {
 		System.out.println("Bonjour à tous promo5");
+		System.out.println("Bonjour à toute la classe");
+		System.out.println("Hello word");
 			}
 }
